43a9ac0e33d303c2732fecd538015c51fb0c94ab
Joshua Henderson <joshhendo@outlook.com>
2013-01-29 13:13:41 +1100

    Added javadoc to the festival class.

--- src/org/roila/Festival.java
+++ src/org/roila/Festival.java
@@ -8,19 +8,30 @@ public class Festival
     String current_voice = null;
 
     // http://stackoverflow.com/questions/2439984/how-to-check-if-a-program-is-installed-on-system
-    private Boolean CheckInstall() throws Exception {
+
+    /**
+     * Checks to see if "festival" is installed.
+     * @return true if festival is installed, otherwise false.
+     */
+    private Boolean CheckInstall() {
         try
         {
             Runtime.getRuntime().exec("festival");
         }
         catch (IOException e)
         {
-            throw new Exception("Festival not installed");
+            //throw new Exception("Festival not installed");
+            return false;
         }
 
         return true;
     }
 
+    /**
+     * Generates an ArrayList<String> of locations where voices could be instaled.
+     * @return ArrayList<String> of voices. If no voices are found this will be null.
+     * @throws Exception if it has an issue running the "find" command on the Linux system.
+     */
     private ArrayList<String> VoiceLocations() throws Exception
     {
         ArrayList<String> locations = new ArrayList<String>();
@@ -49,6 +60,12 @@ public class Festival
         return locations;
     }
 
+    /**
+     * Checks a voice is installed in an appropriate directory. The paramater is case sensitive.
+     * @param name The name of the folder of the voice to check. This is case sensitive.
+     * @return The first identified path of the voice, otherwise null.
+     * @throws Exception if it has an issue running VoiceLocations()
+     */
     public String CheckVoice(String name) throws Exception
     {
         ArrayList<String> locations = VoiceLocations();
@@ -62,66 +79,65 @@ public class Festival
         return null;
     }
 
-    private void SetVoice(String voice)
+    /**
+     * Sets the current voice to a particular voice name.
+     * @param voice name of the folder that the voice is contained in.
+     * @return true if the voice is successfully changed, otherwise false.
+     */
+    private boolean SetVoice(String voice)
     {
-        ArrayList<String> locations = null;
-
+        String voice_location = null;
         try
         {
-            locations = VoiceLocations();
+            voice_location = CheckVoice(voice);
         }
         catch (Exception e)
         {
-            return;
+            return false;
         }
 
-        if (locations == null || locations.isEmpty()) return;
+        if (voice_location == null) return false;
 
-        String location = locations.get(0);
-
-        String female = "roila_female";
-        String male = "roila_diphone";
-
-        String check = null;
-
-        if (voice == female)
-        {
-            check = female;
-        }
-        else if (voice == male)
-        {
-            check = male;
-        }
-        else
-        {
-            return;
-        }
-
-        File file = new File(location + "/" + check);
+        File file = new File(voice_location);
 
         if (file.exists())
         {
-            current_voice = check;
+            current_voice = voice;
+            return true;
         }
 
-        return;
+        return false;
     }
 
+    /**
+     * Public method using SetVoice to change to the male voice.
+     */
     public void SetMale()
     {
         SetVoice("roila_diphone");
     }
 
+    /**
+     * Public method using SetVoice to change to the female voice.
+     */
     public void SetFemale()
     {
         SetVoice("roila_female");
     }
 
+    /**
+     * Public mehtod to clear any set voice and use the default.
+     */
     public void ClearVoice()
     {
         current_voice = null;
     }
 
+    /**
+     * Say something using festival. Will use the most recently set voice.
+     * @param input Sentence or series of words to say.
+     * @throws Exception
+     */
     public void Say(String input) throws Exception
     {
         CheckInstall();
@@ -150,17 +166,12 @@ public class Festival
 
     }
 
+    /**
+     * Public version of CheckInstall.
+     * @return true if festival is available on the system, otherwise false;
+     */
     public Boolean IsAvailable()
     {
-        try
-        {
-            CheckInstall();
-        }
-        catch (Exception e)
-        {
-            return false;
-        }
-
-        return true;
+        return CheckInstall();
     }
 }
